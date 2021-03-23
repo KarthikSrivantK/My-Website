63609522aaef6c19f0d10f9c0cfba1e389c3aafc
Karthik Srivant K <74503930+KarthikSrivantK@users.noreply.github.com>
2021-03-23 14:31:18 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6059a965f029cd001f5e30ec",
+				"6059a965f029cd001f5e30ee"
+			]
+		},
+		"6059a965f029cd001f5e30ec": {
+			"id": "6059a965f029cd001f5e30ec",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6059a965f029cd001f5e30ee": {
+			"id": "6059a965f029cd001f5e30ee",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
